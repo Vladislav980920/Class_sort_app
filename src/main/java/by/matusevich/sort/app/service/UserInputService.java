535b752c2d2d43b496f94b257357b6c6b36fcc42
FireNike2002 <114804266+FireNike2002@users.noreply.github.com>
2026-02-19 18:31:50 +0400

    Update UserInputService.java
    Добавил сервис ввода

--- src/main/java/by/matusevich/sort/app/service/UserInputService.java
+++ src/main/java/by/matusevich/sort/app/service/UserInputService.java
@@ -1,4 +1,38 @@
 package by.matusevich.sort.app.service;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class UserInputService {
+    private Scanner userScan;
+    public UserInputService(){
+        this.userScan = new Scanner(System.in);
+    }
+    public int readInt(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            if (userScan.hasNextInt()) {
+                int value = userScan.nextInt();
+                userScan.nextLine();
+                return value;
+            } else {
+                System.out.println("Ошибка: введите целое число.");
+                userScan.next();
+            }
+        }
+    }
+
+    public String readString(String prompt) {
+        System.out.print(prompt);
+        return userScan.nextLine();
+    }
+
+    public String readLineTrim(String prompt) {
+        System.out.print(prompt);
+        return userScan.nextLine().trim();
+    }
+
+    public void close() {
+        userScan.close();
+    }
 }
